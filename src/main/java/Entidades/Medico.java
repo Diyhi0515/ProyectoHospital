a0d9546cb06a98c1af58d77db91143dc8b1ae586
Diyhi0515 <120338163+Diyhi0515@users.noreply.github.com>
2023-07-01 21:49:17 -0400

    Consultas para Medico

--- src/main/java/Entidades/Medico.java
+++ src/main/java/Entidades/Medico.java
@@ -1,17 +1,28 @@
 package Entidades;
 
-public class Medico {
-    private int ci;
+import Conexion.ConectarBase;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class Medico extends Persona{
+    private int per_ci;
     private int nLicMedica;
     private String especialidad;
-    public Medico(int ci, int nLicMedica, String especialidad){
-        this.ci = ci;
+    public Medico(int ci, String nombre, String apellidoP, String apellidoM, float salario, Date fechaContatacion, int nLicMedica, String especialidad){
+        super(ci, nombre, apellidoP, apellidoM, salario, fechaContatacion);
+        this.per_ci = ci;
         this.nLicMedica = nLicMedica;
         this.especialidad = especialidad;
     }
+    public Medico(){
+    }
 
-    public int getCi() {
-        return ci;
+    public int getPer_Ci() {
+        return per_ci;
     }
 
     public int getnLicMedica() {
@@ -22,8 +33,8 @@ public class Medico {
         return especialidad;
     }
 
-    public void setCi(int ci) {
-        this.ci = ci;
+    public void setPer_Ci(int per_ci) {
+        this.per_ci = per_ci;
     }
 
     public void setnLicMedica(int nLicMedica) {
@@ -32,6 +43,62 @@ public class Medico {
 
     public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
+    }
+
+    public static Medico getMedico(int id){
+        ConectarBase conexion =new ConectarBase();
+        Medico md = new Medico();
+        try (Connection con = conexion.conectarMySQL()) {
+            PreparedStatement consulta = con.prepareStatement("SELECT * FROM Medico WHERE per_ci = ?");
+            consulta.setInt(1,id);
+            ResultSet rs=consulta.executeQuery();
+            while(rs.next()){
+                md.setPer_Ci(rs.getInt("per_ci"));
+                md.setnLicMedica(rs.getInt("nLicMedica"));
+                md.setEspecialidad(rs.getString("especialidad"));
+            }
+            rs.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return md;
+    }
+    public void eliminarMedico(int id){
+        ConectarBase con = new ConectarBase();
+        try {
+            PreparedStatement ps = con.conectarMySQL().prepareStatement(
+                    "delete from Medico where per_ci = ?");
+            ps.setInt(1,id);
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void modificarNLicMedica(int ci, int n){
+        ConectarBase con = new ConectarBase();
+        try {
+            PreparedStatement ps = con.conectarMySQL().prepareStatement(
+                    "UPDATE Medico  \n" +
+                            "    SET nLicMedica = ?   where per_ci = ?");
+            ps.setInt(1, n);
+            ps.setInt(2,ci);
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void modificarEspecialidad(int ci, String n){
+        ConectarBase con = new ConectarBase();
+        try {
+            PreparedStatement ps = con.conectarMySQL().prepareStatement(
+                    "UPDATE Medico  \n" +
+                            "    SET especialidad = ?   where per_ci = ?");
+            ps.setString(1, n);
+            ps.setInt(2,ci);
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
